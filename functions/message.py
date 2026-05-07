473c064885e4acba2af6e2b9e79d9e897773a79c
neshy02 <websonkkq@gmail.com>
2026-05-07 16:15:14 +0300

    feat: solved the problem with the massage display function

---
+++ functions/message.py
@@ -0,0 +1,7 @@
+# https://stepik.org/lesson/567052/step/6?unit=561326
+
+def solved_problem():
+    name, surname = input().split()
+    print(f'Уважаемый, {name} {surname}! Вы верно выполнили это задание!')
+    
+solved_problem()
